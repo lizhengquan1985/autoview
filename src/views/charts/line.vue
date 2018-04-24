c402a7270c8b3434bad23cfc7cb3cdf3241d73f2
lizhengquan1985 <entergl@163.com>
2018-04-24 16:35:40 +0800

    1
    1

--- src/views/charts/line.vue
+++ src/views/charts/line.vue
@@ -17,7 +17,10 @@
         <el-option value="qtum">qtum</el-option>
         <el-option value="iost">iost</el-option>
         <el-option value="neo">neo</el-option>
+        <el-option value="ont">ada</el-option>
 
+        <el-option value="ont">act</el-option>
+        <el-option value="ont">btm</el-option>
         <el-option value="ont">ont</el-option>
         <el-option value="trx">trx</el-option>
         <el-option value="mds">mds</el-option>
