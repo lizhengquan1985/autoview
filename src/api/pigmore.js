1ae9673901fc033abc164538e96253de10ab8c06
lizhengquan1985 <entergl@163.com>
2018-05-05 19:48:07 +0800

    1

---
+++ src/api/pigmore.js
@@ -0,0 +1,52 @@
+import request from '@/utils/request'
+
+export function fetchLZQDetail() {
+  return request({
+    url: '/spotrecord/detail?username=lzq',
+    method: 'get'
+  })
+}
+export function fetchLZQMarginDetail() {
+  return request({
+    url: '/spotrecord/margindetail?username=lzq',
+    method: 'get'
+  })
+}
+
+export function fetchYXQDetail() {
+  return request({
+    url: '/spotrecord/detail?username=yxq',
+    method: 'get'
+  })
+}
+
+export function fetchArticle() {
+  return request({
+    url: '/article/detail',
+    method: 'get'
+  })
+}
+
+export function fetchPv(pv) {
+  return request({
+    url: '/article/pv',
+    method: 'get',
+    params: { pv }
+  })
+}
+
+export function createArticle(data) {
+  return request({
+    url: '/article/create',
+    method: 'post',
+    data
+  })
+}
+
+export function updateArticle(data) {
+  return request({
+    url: '/article/update',
+    method: 'post',
+    data
+  })
+}
