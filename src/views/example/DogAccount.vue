991b66ce8021efbe2c05c2060a2160a451850d04
lizhengquan1985 <entergl@163.com>
2018-10-23 10:12:42 +0800

    11

--- src/views/example/DogAccount.vue
+++ src/views/example/DogAccount.vue
@@ -1,48 +1,55 @@
 <template>
   <div style="padding: 5px;">
-    <el-select v-model="order">
-      <el-option label="coin" :value="`coin`"/>
-      <el-option label="canEmptyAmount" :value="`canEmptyAmount`"/>
-    </el-select>
-    <el-checkbox v-model="asc"/>
-    <el-input v-model="userName" style="width: 200px;"/>
-    <el-button size="mini" @click="initAccountInfo()">查询</el-button>
-    <el-table
-      border
-      size="mini"
-      :data="list"
-      style="width: 100%">
-      <el-table-column
-        prop="currency"
-        label="名称"
-        width="75">
-      </el-table-column>
-      <el-table-column
-        prop="buyQuantity"
-        label="总狗"
-        width="165">
-      </el-table-column>
-      <el-table-column
-        prop="balance"
-        label="余啊"
-        width="145">
-      </el-table-column>
-      <el-table-column
-        prop="canEmptyQuantity"
-        label="剩Q"
-        width="155">
-      </el-table-column>
-      <el-table-column
-        prop="canEmptyAmount"
-        label="剩M"
-        width="155">
-      </el-table-column>
-      <el-table-column
-        prop="nowPrice"
-        label="nowP"
-        width="145">
-      </el-table-column>
-    </el-table>
+    <el-card>
+      <el-select size="mini" v-model="sort">
+        <el-option label="default" :value="`default`"/>
+        <el-option label="currencyasc" :value="`currencyasc`"/>
+        <el-option label="currencydesc" :value="`currencydesc`"/>
+        <el-option label="canEmptyAmountasc" :value="`canEmptyAmountasc`"/>
+        <el-option label="canEmptyAmountdesc" :value="`canEmptyAmountdesc`"/>
+      </el-select>
+      <el-input size="mini" v-model="userName" style="width: 80px;" @click.native="changeUserName"/>
+      <el-input size="mini" v-model="quoteCurrency" style="width: 100px;" @click.native="changeQuoteCurrency"/>
+      <el-button size="mini" @click="initAccountInfo()" type="primary">查询</el-button>
+    </el-card>
+    <div style="margin-top: 5px;">
+      <el-table
+        border
+        size="mini"
+        :data="list"
+        style="width: 100%">
+        <el-table-column
+          prop="currency"
+          label="名称"
+          width="75">
+        </el-table-column>
+        <el-table-column
+          prop="buyQuantity"
+          label="总狗"
+          width="165">
+        </el-table-column>
+        <el-table-column
+          prop="balance"
+          label="余啊"
+          width="145">
+        </el-table-column>
+        <el-table-column
+          prop="canEmptyQuantity"
+          label="剩Q"
+          width="155">
+        </el-table-column>
+        <el-table-column
+          prop="canEmptyAmount"
+          label="剩M"
+          width="155">
+        </el-table-column>
+        <el-table-column
+          prop="nowPrice"
+          label="nowP"
+          width="145">
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
@@ -56,9 +63,9 @@
     name: 'HelloWorld',
     data() {
       return {
-        order: '',
-        asc: true,
-        userName: '',
+        sort: 'canEmptyAmountasc',
+        quoteCurrency: 'usdt',
+        userName: 'qq',
         list: [],
       };
     },
@@ -68,25 +75,33 @@
     methods: {
       initAccountInfo: function() {
         console.log(this.order);
-        const userName = this.userName;
-        if (!userName) {
+        const {userName, sort, quoteCurrency} = this;
+        if (!userName || !quoteCurrency) {
           this.list = [];
           return;
         }
-        if (this.list.length > 0) {
-          this.list.sort((a, b) => {
-            if (this.asc) {
-              return a[this.order] - b[this.order];
-            } else {
-              return b[this.order] - a[this.order];
-            }
-          });
-          return;
-        }
-        initAccountInfo({userName}).then(data => {
+        initAccountInfo({userName, sort, quoteCurrency}).then(data => {
           console.log(data);
           this.list = data.data;
         });
+      },
+      changeUserName() {
+        if (this.userName === 'qq') {
+          this.userName = 'xx';
+        } else if (this.userName === 'xx') {
+          this.userName = 'qq';
+        }
+      },
+      changeQuoteCurrency() {
+        if (this.quoteCurrency === 'usdt') {
+          this.quoteCurrency = 'btc';
+        } else if (this.quoteCurrency === 'btc') {
+          this.quoteCurrency = 'eth';
+        } else if (this.quoteCurrency === 'eth') {
+          this.quoteCurrency = 'ht';
+        } else if (this.quoteCurrency === 'ht') {
+          this.quoteCurrency = 'usdt';
+        }
       },
     },
   };
